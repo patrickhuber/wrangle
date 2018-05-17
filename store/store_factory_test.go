767862d0e599c08f476afa026e65c2642f48c48e
Patrick Huber <patrickhuber@hotmail.com>
2018-05-17 11:03:44 -0400

    updates store factory tests

--- store/store_factory_test.go
+++ store/store_factory_test.go
@@ -6,19 +6,21 @@ import (
 	"github.com/patrickhuber/cli-mgr/config"
 )
 
-func TestStoreFactoryCanCreateMemoryStore(t *testing.T) {
-	configSource := &config.ConfigSource{
-		ConfigSourceType: "memory",
-		Config:           "config",
-		Name:             "name",
-		Params:           map[string]string{}}
-	storeFactory := NewStoreFactory()
-	store := storeFactory.Create(configSource)
-	if store == nil {
-		t.Error("store is nil")
-	}
-	name := store.GetName()
-	if name != configSource.Name {
-		t.Errorf("expected name '%s' actual '%s'", configSource.Name, name)
-	}
+func TestStoreFactory(t *testing.T) {
+	t.Run("CanCreateMemoryStore", func(t *testing.T) {
+		configSource := &config.ConfigSource{
+			ConfigSourceType: "memory",
+			Config:           "config",
+			Name:             "name",
+			Params:           map[string]string{}}
+		storeFactory := NewStoreFactory()
+		store := storeFactory.Create(configSource)
+		if store == nil {
+			t.Error("store is nil")
+		}
+		name := store.GetName()
+		if name != configSource.Name {
+			t.Errorf("expected name '%s' actual '%s'", configSource.Name, name)
+		}
+	})
 }
