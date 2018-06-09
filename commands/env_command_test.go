169336c37b6636f741f0aa9d7cd6071721bd98e0
Patrick Huber <patrick@huber.com>
2018-06-09 14:44:26 -0400

    updates vendor for macos

--- commands/env_command_test.go
+++ commands/env_command_test.go
@@ -4,12 +4,15 @@ import (
 	"flag"
 	"testing"
 
+	"github.com/spf13/afero"
+
 	"github.com/urfave/cli"
 )
 
 func TestEnvCommand(t *testing.T) {
 	t.Run("CanRunCommand", func(t *testing.T) {
-		cmd := NewEnvCommand()
+		fileSystem := afero.NewMemMapFs()
+		cmd := NewEnvCommand(fileSystem)
 
 		app := cli.NewApp()
 		set := &flag.FlagSet{}
