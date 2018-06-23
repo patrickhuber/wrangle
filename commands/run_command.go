4504a88dd084f1dddff68bccc1fd2455360143f6
patrickhuber <patrickhuber@users.noreply.github.com>
2018-06-23 12:33:46 -0400

    updates run command to use store pipeline

--- commands/run_command.go
+++ commands/run_command.go
@@ -1,8 +1,6 @@
 package commands
 
 import (
-	"fmt"
-
 	"github.com/pkg/errors"
 
 	"github.com/patrickhuber/cli-mgr/processes"
@@ -18,55 +16,49 @@ type RunCommand interface {
 }
 
 type runCommand struct {
-	configStoreManager store.Manager
-	fileSystem         afero.Fs
-	processFactory     processes.ProcessFactory
+	manager        store.Manager
+	fileSystem     afero.Fs
+	processFactory processes.ProcessFactory
 }
 
 // NewRunCommand - creates a new run command
 func NewRunCommand(
-	configStoreManager store.Manager,
+	manager store.Manager,
 	fileSystem afero.Fs,
 	processFactory processes.ProcessFactory) RunCommand {
 	return &runCommand{
-		configStoreManager: configStoreManager,
-		fileSystem:         fileSystem,
-		processFactory:     processFactory}
+		manager:        manager,
+		fileSystem:     fileSystem,
+		processFactory: processFactory}
 }
 
 func (cmd *runCommand) ExecuteCommand(params RunCommandParams) error {
 
-	if params.ProcessName() == "" {
+	configFile := params.ConfigFile()
+	processName := params.ProcessName()
+	environmentName := params.EnvironmentName()
+
+	if processName == "" {
 		return errors.New("process name is required for the run command")
 	}
 
-	if params.EnvironmentName() == "" {
+	if environmentName == "" {
 		return errors.New("environment name is required for the run command")
 	}
 
 	configLoader := config.NewConfigLoader(cmd.fileSystem)
-	cfg, err := configLoader.Load(params.ConfigFile())
-	if err != nil {
-		return errors.Wrapf(err, "error loading config from config file '%s'", params.ConfigFile())
-	}
-	if cfg == nil {
-		return errors.New("config is null")
-	}
-	return cmd.executeConfigItem(cfg, params.ProcessName(), params.EnvironmentName())
-}
 
-func (cmd *runCommand) executeConfigItem(cfg *config.Config, processName string, environmentName string) error {
-	for _, p := range cfg.Processes {
-		if p.Name == processName {
-			for _, e := range p.Environments {
-				if e.Name == environmentName {
-					return cmd.execute(&e)
-				}
-			}
-			return fmt.Errorf("unable to find environment '%s' in process '%s'", environmentName, processName)
-		}
+	cfg, err := configLoader.Load(configFile)
+	if err != nil {
+		return errors.Wrap(err, "error running configLoader.Load")
 	}
-	return fmt.Errorf("No Processes found in config that match '%s'", processName)
+	pipeline := store.NewPipeline(cmd.manager, cfg)
+	environment, err := pipeline.Run(processName, environmentName)
+	if err != nil {
+		return err
+	}
+
+	return cmd.execute(environment)
 }
 
 func (cmd *runCommand) execute(processEnvironmentConfig *config.Environment) error {
