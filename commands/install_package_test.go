a5085ea883de400e882d57c2bbf64267d5ba4e5d
patrickhuber <patrickhuber@users.noreply.github.com>
2018-07-03 22:33:11 -0400

    adds platform tests for binary package install

--- commands/install_package_test.go
+++ commands/install_package_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"path/filepath"
 	"testing"
 
 	"github.com/spf13/afero"
@@ -14,8 +15,19 @@ import (
 
 func TestInstallPackageCommand(t *testing.T) {
 	t.Run("CanInstallBinaryPackageOnWindows", func(t *testing.T) {
-		r := require.New(t)
-		content := `
+		canInstallBinaryPackage(t, "windows", "c:\\test")
+	})
+	t.Run("CanInstallBinaryPackageOnLinux", func(t *testing.T) {
+		canInstallBinaryPackage(t, "linux", "/test")
+	})
+	t.Run("CanInstallBinaryPackageOnMac", func(t *testing.T) {
+		canInstallBinaryPackage(t, "darwin", "/test")
+	})
+}
+
+func canInstallBinaryPackage(t *testing.T, platform string, outFolder string) {
+	r := require.New(t)
+	content := `
 packages:
 - name: fly
   version: 3.14.1
@@ -34,34 +46,33 @@ packages:
       url: %s
       out: fly_((version))_darwin_amd64
 `
-		message := "this is a message"
+	message := "this is a message"
 
-		// start the local http server
-		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			rw.Write([]byte(message))
-		}))
+	// start the local http server
+	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		rw.Write([]byte(message))
+	}))
 
-		// close connection when test is finished
-		defer server.Close()
+	// close connection when test is finished
+	defer server.Close()
 
-		// replace the url in the content with the test server url
-		content = fmt.Sprintf(content, server.URL, server.URL, server.URL)
+	// replace the url in the content with the test server url
+	content = fmt.Sprintf(content, server.URL, server.URL, server.URL)
 
-		// serialize the config to a config object
-		cfg, err := config.SerializeString(content)
-		r.Nil(err)
+	// serialize the config to a config object
+	cfg, err := config.SerializeString(content)
+	r.Nil(err)
 
-		// create the filesystem and command
-		fileSystem := afero.NewMemMapFs()
-		command := NewInstallPackage("windows", "c:\\test", fileSystem)
+	// create the filesystem and command
+	fileSystem := afero.NewMemMapFs()
+	command := NewInstallPackage(platform, outFolder, fileSystem)
 
-		// execute
-		err = command.Execute(&cfg.Packages[0])
-		r.Nil(err)
+	// execute
+	err = command.Execute(&cfg.Packages[0])
+	r.Nil(err)
 
-		// verify downloaded file extists
-		ok, err := afero.Exists(fileSystem, "c:\\test\\fly_3.14.1_windows_amd64")
-		r.Nil(err)
-		r.True(ok)
-	})
+	// verify downloaded file extists
+	ok, err := afero.Exists(fileSystem, filepath.Join(outFolder, fmt.Sprintf("fly_3.14.1_%s_amd64", platform)))
+	r.Nil(err)
+	r.True(ok)
 }
