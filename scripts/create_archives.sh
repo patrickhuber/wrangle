94c89229998b81f32e0e0382782546679afc43b9
patrickhuber <patrickhuber@users.noreply.github.com>
2018-07-08 23:51:27 -0400

    adds archive script

---
+++ scripts/create_archives.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+tar czvf cli-mgr-darwin-amd64.tgz cli-mgr-darwin-amd64
+tar czvf cli-mgr-windows-amd64.tgz cli-mgr-windows-amd64.exe
+tar czvf cli-mgr-linux-amd64.tgz cli-mgr-linux-amd64
