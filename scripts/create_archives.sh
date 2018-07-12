ceddce0c068f3d3ae3640b3a679dbe8c57ac0bd2
patrickhuber <patrickhuber@users.noreply.github.com>
2018-07-12 02:02:42 -0400

    adds zip archive to the list of generated archives

--- scripts/create_archives.sh
+++ scripts/create_archives.sh
@@ -2,3 +2,4 @@
 tar czvf wrangle-darwin-amd64.tgz wrangle-darwin-amd64
 tar czvf wrangle-windows-amd64.tgz wrangle-windows-amd64.exe
 tar czvf wrangle-linux-amd64.tgz wrangle-linux-amd64
+zip -9 wrangle-windows-amd64.zip wrangle-windows-amd64.exe
