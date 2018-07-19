6366d75a0f97458fabee2bacfacfec096ab22b67
patrickhuber <patrickhuber@users.noreply.github.com>
2018-07-19 14:46:07 -0400

    bumps release to 0.5.0

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ func createApplication(
 	cliApp.Usage = "a cli management tool"
 	cliApp.Writer = console.Out()
 	cliApp.ErrWriter = console.Error()
-	cliApp.Version = "0.4.7"
+	cliApp.Version = "0.5.0"
 
 	cliApp.Flags = []cli.Flag{
 		cli.StringFlag{
