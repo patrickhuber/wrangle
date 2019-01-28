13f597f2c932fea184d626f031fe61d625ac9f58
patrickhuber <patrickhuber@users.noreply.github.com>
2019-01-28 12:58:25 -0500

    updates version; makes archive script portable

--- scripts/create_archives.sh
+++ scripts/create_archives.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-tar czvf wrangle-darwin-amd64.tgz wrangle-darwin-amd64
-tar czvf wrangle-windows-amd64.tgz wrangle-windows-amd64.exe
-tar czvf wrangle-linux-amd64.tgz wrangle-linux-amd64
-zip -9 wrangle-windows-amd64.zip wrangle-windows-amd64.exe
+
+# get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BIN=$(realpath $DIR/../bin)
+pushd $BIN
+    tar czvf wrangle-darwin-amd64.tgz wrangle-darwin-amd64
+    tar czvf wrangle-windows-amd64.tgz wrangle-windows-amd64.exe
+    tar czvf wrangle-linux-amd64.tgz wrangle-linux-amd64
+    zip -9 wrangle-windows-amd64.zip wrangle-windows-amd64.exe
+popd
