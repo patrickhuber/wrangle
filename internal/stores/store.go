75bfa28a6692872d150a755be74b45369c5b39a8
patrickhuber <patrickhuber@users.noreply.github.com>
2024-08-17 09:44:12 -0400

    adds platform matrix; splits store into reader and writer

--- internal/stores/store.go
+++ internal/stores/store.go
@@ -1,7 +1,17 @@
 package stores
 
 type Store interface {
+	Readable
+	Writable
+}
+
+// Readable store is the read interface for Store
+type Readable interface {
 	Get(k Key) (any, bool, error)
-	Set(k Key, v any) error
 	List() ([]Key, error)
+}
+
+// Writeable store is the write interface for Store
+type Writable interface {
+	Set(k Key, v any) error
 }
