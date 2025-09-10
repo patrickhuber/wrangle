b00ea5f5d29f4ffb12cc26f70ccf0d9f8fcbc949
patrickhuber <patrickhuber@users.noreply.github.com>
2025-09-09 22:54:12 -0400

    adds export tests

--- internal/export/export_test.go
+++ internal/export/export_test.go
@@ -1,7 +1,86 @@
 package export_test
 
-import "testing"
+import (
+	"bytes"
+	"testing"
 
-func TestExport(t *testing.T) {
+	"github.com/patrickhuber/go-cross/console"
+	"github.com/patrickhuber/go-shellhook"
+	"github.com/patrickhuber/wrangle/internal/envdiff"
+	"github.com/patrickhuber/wrangle/internal/export"
+	"github.com/patrickhuber/wrangle/internal/global"
+	"github.com/stretchr/testify/require"
+)
 
+func TestService_Execute_WritesShellCommandsAndDiff(t *testing.T) {
+	shells := map[string]shellhook.Shell{
+		shellhook.Bash:       shellhook.NewBash(),
+		shellhook.Powershell: shellhook.NewPowershell(),
+	}
+
+	type testCase struct {
+		name       string
+		shell      string
+		changes    []envdiff.Change
+		expectSubs []string
+	}
+
+	cases := []testCase{
+		{
+			name:  "bash_changes",
+			shell: shellhook.Bash,
+			changes: []envdiff.Change{
+				envdiff.Add{Key: "FOO", Value: "bar"},
+				envdiff.Update{Key: "FOO", Value: "baz"},
+				envdiff.Remove{Key: "BAR"},
+			},
+			expectSubs: []string{
+				"export FOO='bar'",
+				"export FOO='baz'",
+				"unset BAR",
+				"export " + global.EnvDiff + "='",
+			},
+		},
+		{
+			name:  "pwsh_changes",
+			shell: shellhook.Powershell,
+			changes: []envdiff.Change{
+				envdiff.Add{Key: "FOO", Value: "bar"},
+				envdiff.Update{Key: "FOO", Value: "baz"},
+				envdiff.Remove{Key: "BAR"},
+			},
+			expectSubs: []string{
+				"$env:FOO=\"bar\"",
+				"$env:FOO=\"baz\"",
+				"Remove-Item Env:\\BAR",
+				"$env:" + global.EnvDiff + "=",
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			con := console.NewMemory()
+			svc := export.NewService(shells, con)
+
+			err := svc.Execute(tc.shell, tc.changes)
+			require.NoError(t, err)
+
+			out := con.Out().(*bytes.Buffer).String()
+			require.NotEmpty(t, out)
+
+			for _, sub := range tc.expectSubs {
+				require.Contains(t, out, sub)
+			}
+		})
+	}
+}
+
+func TestService_Execute_InvalidShell(t *testing.T) {
+	shells := map[string]shellhook.Shell{shellhook.Bash: shellhook.NewBash()}
+	con := console.NewMemory()
+	svc := export.NewService(shells, con)
+
+	err := svc.Execute("zsh", nil)
+	require.Error(t, err)
 }
